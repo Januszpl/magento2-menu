78a75a1a49e8ac1474753c2eca62dccf7436594a
Janusz Janczy <janusz.janczy@snow.dog>
2020-09-15 00:08:24 -0300

    #84: add changes from develop to resolve conflicts only for frontend files so backend can resolve only backend conflicts

--- view/adminhtml/web/vue/field-type/autocomplete.vue
+++ view/adminhtml/web/vue/field-type/autocomplete.vue
@@ -4,97 +4,101 @@
             {{ label }}
         </label>
         <div class="admin__field-control control">
+            <!-- TODO: re-add changes from #84 -->
             <v-select
                 v-model="selected"
                 :options="options"
-                :placeholder="placeHolder"
+                :placeholder="placeholder"
+                :clearable="false"
             >
+                <template v-slot:option="option">
+                    {{ option.label }}
+
+                    <template v-if="option.store.length > 0">
+                        <span class="vs__dropdown-option__details">
+                            {{ option.store.join(', ') }}
+                        </span>
+                    </template>
+                </template>
             </v-select>
-        </div>
-        <div class="selected-option">
-            <div class="selected-option__label">
-                {{ description }}
-            </div>
-            <div class="selected-option__value">
-                <span v-if="item[itemKey]">
-                    {{ item[itemKey] }}
-                </span>
-                <span v-else>
-                    {{ placeHolder }} ⇡
-                </span>
-            </div>
         </div>
     </div>
 </template>
+
 <script>
-define(["Vue"], function(Vue) {
-    Vue.component("auto-complete", {
-        template: template,
-        props: {
-            label: {
-                type: String
-            },
-            description: {
-                type: String
-            },
-            options: {
-                type: Array
-            },
-            item: {
-                type: Object
-            },
-            config: {
-                type: Object
-              },
-            itemKey: {
-                type: String,
-                default: 'content'
-            },
-            defaultOptionValue: {
-                type: String,
-                default: 'default'
-            }
-        },
-        data () {
-          return {
-              defaultSelectedOption: false
-          }
-        },
-        computed: {
-            selected: {
-                get() {
-                    var selectedOption = '',
-                        optionValue;
-
-                    for (var i = 0; i < this.options.length; i++) {
-                        optionValue = this.options[i].value.toString();
-                        if (optionValue === this.item[this.itemKey]) {
-                            selectedOption = this.options[i];
-                        }
-                        if (optionValue === this.defaultOptionValue) {
-                            this.defaultSelectedOption = this.options[i];
-                        }
-                    }
-
-                    if (!selectedOption) {
-                        selectedOption = this.defaultSelectedOption;
-                    }
-
-                    return selectedOption;
+    define(['Vue'], function(Vue) {
+        Vue.component('auto-complete', {
+            props: {
+                label: {
+                    type: String,
+                    required: true
                 },
-                set(option) {
-                    if (option && typeof option === 'object') {
-                        this.item[this.itemKey] = option.value.toString();
-                    }
-                    else {
-                      this.item[this.itemKey] = this.defaultSelectedOption ? this.defaultSelectedOption.value.toString() : '';
-                    }
+                description: {
+                    type: String,
+                    required: true
+                },
+                options: {
+                    type: Array,
+                    required: true
+                },
+                item: {
+                    type: Object,
+                    required: true
+                },
+                config: {
+                    type: Object,
+                    required: true
+                },
+                itemKey: {
+                    type: String,
+                    default: 'content'
+                },
+                defaultOptionValue: {
+                    type: String,
+                    default: 'default'
                 }
             },
-            placeHolder: function() {
-                return this.config.translation.pleaseSelect + " " + this.label.toLocaleLowerCase();
-            }
-        }
+            data () {
+              return {
+                  defaultSelectedOption: false
+              }
+            },
+            computed: {
+                selected: {
+                    get() {
+                        var selectedOption = '',
+                            optionValue;
+
+                        for (var i = 0; i < this.options.length; i++) {
+                            optionValue = this.options[i].value.toString();
+                            if (optionValue === this.item[this.itemKey]) {
+                                selectedOption = this.options[i];
+                            }
+                            if (optionValue === this.defaultOptionValue) {
+                                this.defaultSelectedOption = this.options[i];
+                            }
+                        }
+
+                        if (!selectedOption) {
+                            selectedOption = this.defaultSelectedOption;
+                        }
+
+                        return selectedOption;
+                    },
+                    set(option) {
+                        if (option && typeof option === 'object') {
+                            this.item[this.itemKey] = option.value.toString();
+                        }
+                        else {
+                          this.item[this.itemKey] = this.defaultSelectedOption ? this.defaultSelectedOption.value.toString() : '';
+                        }
+                    }
+                },
+                placeHolder: function() {
+                    return this.config.translation.pleaseSelect + ' ' + this.label.toLocaleLowerCase();
+                },
+            },
+            template: template
+        });
     });
-});
 </script>
--- view/adminhtml/web/vue/menu-type.vue
+++ view/adminhtml/web/vue/menu-type.vue
@@ -1,19 +1,13 @@
 <template>
     <fieldset class="admin__fieldset fieldset-wide">
-        <simple-field
-            :label="config.translation.nodeName"
-            id="node_name"
-            type="textarea"
-            v-model="item.title"
+         <checkbox
+            :label="isNodeActiveLabel"
+            id="is_active"
+            :item="item"
+            :value="item.is_active"
         >
-        </simple-field>
-        <simple-field
-            :label="config.translation.nodeClasses"
-            id="node_classes"
-            type="text"
-            v-model="item.classes"
-        >
-        </simple-field>
+        </checkbox>
+
         <div class="admin__field field field-title">
             <label
                 class="label admin__field-label"
@@ -24,19 +18,37 @@
             <div class="admin__field-control control">
                 <v-select
                     :value="item.type"
-                    @input="changeType"
                     :options="options"
                     :placeholder="config.translation.selectNodeType"
-                    :getOptionLabel="getOptionLabel"
                     :searchable="false"
-                >
-                </v-select>
+                    :get-option-label="getOptionLabel"
+                    :clearable="false"
+                    @input="changeType"
+                />
             </div>
         </div>
         <h2>
             {{ additionalLabel }}
         </h2>
-        <component :is="item['type']" :item="item" :config="config"></component>
+        <component
+            :is="item['type']"
+            :item="item"
+            :config="config"
+        />
+
+        <simple-field
+            id="node_name"
+            v-model="item.title"
+            :label="config.translation.nodeName"
+            type="textarea"
+        />
+
+        <simple-field
+            id="node_classes"
+            v-model="item.classes"
+            :label="config.translation.nodeClasses"
+            type="text"
+        />
         <h2>
             {{ templatesLabel }}
         </h2>
@@ -65,76 +77,86 @@
 </template>
 
 <script>
-define(["Vue", "mage/translate"], function(Vue, $t) {
-    Vue.component("snowdog-menu-type", {
-        template: template,
-        props: ['item', 'config'],
-        data: function() {
-            return {
-                draft: {},
-                additionalLabel: $t('Additional type options'),
-                noTemplatesMessage: $t('There is no custom templates defined in theme for this type of node.'),
-                templatesLabel: $t('Templates'),
-                templateList: {
-                  'node': 'snowMenuNodeCustomTemplates',
-                  'submenu': 'snowMenuSubmenuCustomTemplates',
+    define(["Vue", "mage/translate"], function(Vue, $t) {
+        Vue.component("snowdog-menu-type", {
+            props: {
+                item: {
+                    type: Object,
+                    required: true
+                },
+                config: {
+                    type: Object,
+                    required: true
                 }
-            }
-        },
-        computed: {
-            isTemplateSectionVisible: function() {
-                var nodeId = this.templateList['node'],
-                    submenuId = this.templateList['submenu'],
-                    typeData = this.config.fieldData[this.item['type']];
-
-                if (typeData[nodeId] || typeData[submenuId]) {
-                    return typeData[nodeId].options.length > 1 || typeData[submenuId].options.length > 1;
-                }
-
-                return false;
             },
-            options: function() {
-                var list = [];
-                for (type in this.config.nodeTypes) {
-                    list.push({
-                        label: this.config.nodeTypes[type],
-                        value: type
-                    })
-                }
-                return list;
-            },
-            templateOptions: function() {
-                return this.templateOptionsData[this.item['type']] || [];
-            }
-        },
-        methods: {
-            changeType: function(selected) {
-                if (selected && typeof selected === 'object') {
-                    var type  = this.item.type,
-                        value = selected.value;
-                    if (type) {
-                        this.draft[type] = {
-                            content: this.item['content']
-                        };
+            data: function() {
+                return {
+                    draft: {},
+                    additionalLabel: $t('Additional type options'),
+                    isNodeActiveLabel: $t('Enabled'),
+                    noTemplatesMessage: $t('There is no custom templates defined in theme for this type of node.'),
+                    templatesLabel: $t('Templates'),
+                    templateList: {
+                      'node': 'snowMenuNodeCustomTemplates',
+                      'submenu': 'snowMenuSubmenuCustomTemplates',
                     }
-                    if (this.draft[value]) {
-                        this.item['content'] = this.draft[value].content;
-                    } else {
-                        this.item['content'] = null;
-                    }
-                    this.item['type'] = value;
                 }
             },
-            getOptionLabel: function(option) {
-                if (typeof option === 'object') {
-                    return option.label;
+            computed: {
+                isTemplateSectionVisible: function() {
+                    var nodeId = this.templateList['node'],
+                        submenuId = this.templateList['submenu'],
+                        typeData = this.config.fieldData[this.item['type']];
+
+                    if (typeData[nodeId] || typeData[submenuId]) {
+                        return typeData[nodeId].options.length > 1 || typeData[submenuId].options.length > 1;
+                    }
+
+                    return false;
+                },
+                options: function() {
+                    var list = [];
+                    for (type in this.config.nodeTypes) {
+                        list.push({
+                            label: this.config.nodeTypes[type],
+                            value: type
+                        })
+                    }
+                    return list;
+                },
+                templateOptions: function() {
+                    return this.templateOptionsData[this.item['type']] || [];
                 }
-                if (option) {
-                    return this.config.nodeTypes[option];
+            },
+            methods: {
+                changeType: function(selected) {
+                    if (selected && typeof selected === 'object') {
+                        var type  = this.item.type,
+                            value = selected.value;
+                        if (type) {
+                            this.draft[type] = {
+                                content: this.item['content']
+                            };
+                        }
+                        if (this.draft[value]) {
+                            this.item['content'] = this.draft[value].content;
+                        } else {
+                            this.item['content'] = null;
+                        }
+                        this.item['type'] = value;
+                    }
+                },
+                getOptionLabel: function(option) {
+                    if (typeof option === 'object') {
+                        return option.label;
+                    }
+                    if (option) {
+                        return this.config.nodeTypes[option];
+                    }
+                    return option;
                 }
-                return option;
-            }
-        }
+            },
+            template: template,
+        });
     });
-});
 </script>
